abc1396bddbf8e18c20ea7b365804f1dfd816350
Arty <abarbonov@gmail.com>
2020-02-05 11:23:47 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`App has been started on port ${PORT}...`);
+});
